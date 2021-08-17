72adf82117f07db1d146a115189a849c6f356554
isabel garcia <ing.cg9@gmail.com>
2021-08-17 14:40:15 -0400

    movies

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,7 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :movie, presence: true
   validates :list, presence: true
+  validates :comment, length: { minimum: 6 }
+
+  validates_uniqueness_of :movie_id, :scope => [:list_id]
 end
